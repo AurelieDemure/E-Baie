4cd8de880ad7f172cee710341f3403a86b1a2919
TEMPESTINI Terry <terry.tempestini@telecomnancy.eu>
2024-01-08 11:08:36 +0100

    init: db init

--- src/main/java/org/codingweek/EntryPoint.java
+++ src/main/java/org/codingweek/EntryPoint.java
@@ -4,13 +4,21 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.codingweek.model.User;
+import org.codingweek.model.UserDAO;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.ResultSet;
 
 import java.io.IOException;
 
 public class EntryPoint extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(EntryPoint.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(EntryPoint.class.getResource("fxml/hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
@@ -18,6 +26,6 @@ public class EntryPoint extends Application {
     }
 
     public static void main(String[] args) {
-        launch();
-    }
+            launch();
+        }
 }
