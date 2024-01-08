fdc303dfcb9837fde9c95707518e937ba371d7f4
demure1u <aurelie.demure@telecomnancy.eu>
2024-01-08 15:33:47 +0100

    ajout d'une boîte de dialogue pour confirmer la suppression d'un compte

--- src/main/java/org/codingweek/controller/AccountController.java
+++ src/main/java/org/codingweek/controller/AccountController.java
@@ -1,16 +1,42 @@
 package org.codingweek.controller;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
+import javafx.util.StringConverter;
+import javafx.util.converter.LocalDateStringConverter;
 import org.codingweek.ApplicationContext;
 import org.codingweek.model.Page;
 
 import java.net.URL;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 import java.util.ResourceBundle;
 
 public class AccountController extends Controller implements Observeur{
     @FXML
     private Label welcomeText;
+    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy", Locale.FRENCH);
+    private LocalDate date;
+
+    public void DateConvert(LocalDate date) {
+
+    }
+
+    @FXML
+    private void showConfirmationAddDialog() {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Confirmation");
+        alert.setHeaderText("Etes vous sur ?");
+        alert.setContentText("Voulez vous vraiment supprimer votre compte ?");
+
+        alert.showAndWait().ifPresent(response -> {
+            if (response == javafx.scene.control.ButtonType.OK) {
+                // CODE WHEN OK
+            }
+        });
+    }
 
     @Override
     public void refresh() {
