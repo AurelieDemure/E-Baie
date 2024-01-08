240ff97747b843ea2a6e5c6d8f765a3aa9eaa1ab
Kallu-A <lucas.aries@protonmail.com>
2024-01-08 09:50:23 +0100

    init: interface

---
+++ src/main/java/org/codingweek/controller/Observeur.java
@@ -0,0 +1,7 @@
+package org.codingweek.controller;
+
+public interface Observeur {
+
+    public void update();
+
+}
---
+++ src/main/java/org/codingweek/view/LoaderFXML.java
@@ -0,0 +1,11 @@
+package org.codingweek.view;
+
+import javafx.scene.Scene;
+
+import java.io.IOException;
+
+public interface LoaderFXML {
+
+    public Scene loadScene() throws IOException;
+
+}
