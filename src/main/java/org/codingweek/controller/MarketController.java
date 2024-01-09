ad2a3091a88b3f667a0c335beb2a543acb97126d
PierRapt <pierre.aug@orange.fr>
2024-01-09 11:52:10 +0100

    update recherche market type d'annonce

--- src/main/java/org/codingweek/controller/MarketController.java
+++ src/main/java/org/codingweek/controller/MarketController.java
@@ -5,14 +5,12 @@ import java.util.*;
 
 import org.codingweek.db.entity.*;
 import org.codingweek.model.*;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.*;
 import javafx.scene.control.*;
 import javafx.scene.image.*;
-import javafx.scene.input.*;
 import javafx.scene.layout.*;
-
-import static org.codingweek.model.OfferType.LOAN;
-import static org.codingweek.model.OfferType.SERVICE;
 
 public class MarketController extends Controller implements Observeur{
 
@@ -39,21 +37,46 @@ public class MarketController extends Controller implements Observeur{
     private ToggleGroup type;
 
     @FXML
-    void reaserchText(KeyEvent event) {
+    private RadioButton serviceButton;
 
-    }
+    @FXML
+    private RadioButton loanButton;
+
+    @FXML
+    private RadioButton serviceLoanButton;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        listTest.add(new Offer("Un truc", "blablabla", new User("Un", "gens", "blupblup", "hgkvhg", "khgsvdk", "kjhsvdfkjv", "ljhsdf", 4), 100, "LOAN", "Hebdomadaire", "Ugine", "ljhsdgljf"));
+        listTest.add(new Offer("Un truc", "blablabla", new User("Un", "gens", "blupblup", "hgkvhg", "khgsvdk", "kjhsvdfkjv", "ljhsdf", 4), 100, "LOAN", "Hebdomadaire", "Ugine", "ljhsdgljf"));
+        listTest.add(new Offer("Un truc", "blablabla", new User("Un", "gens", "blupblup", "hgkvhg", "khgsvdk", "kjhsvdfkjv", "ljhsdf", 4), 100, "LOAN", "Hebdomadaire", "Ugine", "ljhsdgljf"));
+        listTest.add(new Offer("Un truc", "blablabla", new User("Un", "gens", "blupblup", "hgkvhg", "khgsvdk", "kjhsvdfkjv", "ljhsdf", 4), 100, "SERVICE", "Hebdomadaire", "Ugine", "ljhsdgljf"));
+        listTest.add(new Offer("Un truc", "blablabla", new User("Un", "gens", "blupblup", "hgkvhg", "khgsvdk", "kjhsvdfkjv", "ljhsdf", 4), 100, "SERVICE", "Hebdomadaire", "Ugine", "ljhsdgljf"));
+        listTest.add(new Offer("Un truc", "blablabla", new User("Un", "gens", "blupblup", "hgkvhg", "khgsvdk", "kjhsvdfkjv", "ljhsdf", 4), 100, "SERVICE", "Hebdomadaire", "Ugine", "ljhsdgljf"));
+    
         this.priceChoice.getItems().setAll("Tout type de prix", "Moins de 100 florains", "Entre 100 et 200 florains", "Entre 200 et 300 florains", "Entre 300 et 400 florains", "Entre 400 et 500 florains", "Plus de 500 florains");
         this.priceChoice.getSelectionModel().select(0);
         this.frequencyChoice.getItems().setAll("Tout type de frequence", "Unique", "Journalier", "Hebdomadaire", "Mensuelle", "Annuelle");
         this.frequencyChoice.getSelectionModel().select(0);
         this.sortChoice.getItems().setAll("Prix croissant", "Prix décroissant", "Titre A-Z", "Titre Z-A", "Auteur A-Z", "Auteur Z-A");
         this.sortChoice.getSelectionModel().select(0);
+        refresh();
+    }
+
+    @Override
+    public void update() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'update'");
+    }
+
+    @Override
+    public void refresh() {
+        for(Offer offer : listTest){
+            this.offers.add(offer);
+        }
+        this.scrollField.getChildren().clear();
         Pane pane;
         Pane spacePane;
-        searchOffers();
         int x = 0;
         HBox hbox = new HBox();
         for(Offer offer : this.offers){
@@ -74,38 +97,40 @@ public class MarketController extends Controller implements Observeur{
             }
         }
         this.scrollField.getChildren().add(hbox);
-
-        listTest.add(new Offer("Un truc", "blablabla", new User("Un", "gens", "blupblup", "hgkvhg", "khgsvdk", "kjhsvdfkjv", "ljhsdf", 4), 100, "LOAN", "Hebdomadaire", "Ugine", "ljhsdgljf"));
-        listTest.add(new Offer("Un truc", "blablabla", new User("Un", "gens", "blupblup", "hgkvhg", "khgsvdk", "kjhsvdfkjv", "ljhsdf", 4), 100, "LOAN", "Hebdomadaire", "Ugine", "ljhsdgljf"));
-        listTest.add(new Offer("Un truc", "blablabla", new User("Un", "gens", "blupblup", "hgkvhg", "khgsvdk", "kjhsvdfkjv", "ljhsdf", 4), 100, "LOAN", "Hebdomadaire", "Ugine", "ljhsdgljf"));
-        listTest.add(new Offer("Un truc", "blablabla", new User("Un", "gens", "blupblup", "hgkvhg", "khgsvdk", "kjhsvdfkjv", "ljhsdf", 4), 100, "LOAN", "Hebdomadaire", "Ugine", "ljhsdgljf"));
-        listTest.add(new Offer("Un truc", "blablabla", new User("Un", "gens", "blupblup", "hgkvhg", "khgsvdk", "kjhsvdfkjv", "ljhsdf", 4), 100, "LOAN", "Hebdomadaire", "Ugine", "ljhsdgljf"));
-        listTest.add(new Offer("Un truc", "blablabla", new User("Un", "gens", "blupblup", "hgkvhg", "khgsvdk", "kjhsvdfkjv", "ljhsdf", 4), 100, "LOAN", "Hebdomadaire", "Ugine", "ljhsdgljf"));
     }
 
-    @Override
-    public void update() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'update'");
+    @FXML
+    void loanSelected(ActionEvent event) {
+        for(Offer offer : listTest){
+            if(offer.getTypeOffer()==OfferType.LOAN){
+                this.offers.add(offer);
+            }
+            else{
+                this.offers.remove(offer);
+            }
+        }
+        refresh();
     }
 
-    @Override
-    public void refresh() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'refresh'");
+    @FXML
+    void serviceLoanSelected(ActionEvent event) {
+        for(Offer offer : listTest){
+            this.offers.add(offer);
+        }
+        refresh();
     }
 
-    public void searchOffers(){
-        Offer offer1 = new Offer();
-        offer1.setType(OfferType.LOAN);
-        offer1.setTitle("Un truc");
-        offer1.setPrice(100);
-        Offer offer2 = new Offer();
-        offer2.setType(OfferType.SERVICE);
-        offer2.setTitle("Un autre truc");
-        offer2.setPrice(10);
-        offers.add(offer1);
-        offers.add(offer2);
+    @FXML
+    void serviceSelected(ActionEvent event) {
+        for(Offer offer : listTest){
+            if(offer.getTypeOffer()==OfferType.SERVICE){
+                this.offers.add(offer);
+            }
+            else{
+                this.offers.remove(offer);
+            }
+        }
+        refresh();
     }
 
     public Pane makePaneOffre(Offer offer){
