a7ee69ba58c4ad7ebd73b2ed2beca65e64864fee
PierRapt <pierre.aug@orange.fr>
2024-01-09 10:19:50 +0100

    update du visuel de market

--- src/main/java/org/codingweek/controller/MarketController.java
+++ src/main/java/org/codingweek/controller/MarketController.java
@@ -4,9 +4,8 @@ import java.net.URL;
 import java.util.*;
 
 import org.codingweek.model.Offer;
-import org.codingweek.model.OfferType;
+import org.codingweek.model.User;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
@@ -17,11 +16,12 @@ import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
-import org.codingweek.model.ImageHandler;
 
 public class MarketController extends Controller implements Observeur{
 
     private List<Offer> offers = new ArrayList<Offer>();
+
+    private List<Offer> listTest = new ArrayList<Offer>();
 
     @FXML
     private ChoiceBox<String> priceChoice;
@@ -50,16 +50,40 @@ public class MarketController extends Controller implements Observeur{
     public void initialize(URL location, ResourceBundle resources) {
         this.priceChoice.getItems().setAll("Tout type de prix", "Moins de 100 florains", "Entre 100 et 200 florains", "Entre 200 et 300 florains", "Entre 300 et 400 florains", "Entre 400 et 500 florains", "Plus de 500 florains");
         this.priceChoice.getSelectionModel().select(0);
-        this.frequencyChoice.getItems().setAll("Tout type de fréquence", "Unique", "Journalier", "Hebdomadaire", "Mensuelle", "Annuelle");
+        this.frequencyChoice.getItems().setAll("Tout type de frequence", "Unique", "Journalier", "Hebdomadaire", "Mensuelle", "Annuelle");
         this.frequencyChoice.getSelectionModel().select(0);
         this.sortChoice.getItems().setAll("Prix croissant", "Prix décroissant", "Titre A-Z", "Titre Z-A", "Auteur A-Z", "Auteur Z-A");
         this.sortChoice.getSelectionModel().select(0);
         Pane pane;
+        Pane spacePane;
         searchOffers();
+        int x = 0;
+        HBox hbox = new HBox();
         for(Offer offer : this.offers){
             pane = makePaneOffre(offer);
-            this.scrollField.getChildren().add(pane);
+            hbox.getChildren().add(pane);
+            spacePane = new Pane();
+            spacePane.getStyleClass().add("space");
+            hbox.getChildren().add(spacePane);
+            x++;
+
+            if(x == 3){
+                this.scrollField.getChildren().add(hbox);
+                spacePane = new Pane();
+                spacePane.getStyleClass().add("space");
+                this.scrollField.getChildren().add(spacePane);
+                hbox = new HBox();
+                x = 0;
+            }
         }
+        this.scrollField.getChildren().add(hbox);
+
+        listTest.add(new Offer("Un truc", "blablabla", new User("Un", "gens", "blupblup", "hgkvhg", 4), 100, "LOAN", "Hebdomadaire", "Ugine", "ljhsdgljf"));
+        listTest.add(new Offer("Un truc", "blablabla", new User("Un", "gens", "blupblup", "hgkvhg", 4), 100, "LOAN", "Hebdomadaire", "Ugine", "ljhsdgljf"));
+        listTest.add(new Offer("Un truc", "blablabla", new User("Un", "gens", "blupblup", "hgkvhg", 4), 100, "LOAN", "Hebdomadaire", "Ugine", "ljhsdgljf"));
+        listTest.add(new Offer("Un truc", "blablabla", new User("Un", "gens", "blupblup", "hgkvhg", 4), 100, "LOAN", "Hebdomadaire", "Ugine", "ljhsdgljf"));
+        listTest.add(new Offer("Un truc", "blablabla", new User("Un", "gens", "blupblup", "hgkvhg", 4), 100, "LOAN", "Hebdomadaire", "Ugine", "ljhsdgljf"));
+        listTest.add(new Offer("Un truc", "blablabla", new User("Un", "gens", "blupblup", "hgkvhg", 4), 100, "LOAN", "Hebdomadaire", "Ugine", "ljhsdgljf"));
     }
 
     @Override
@@ -75,16 +99,11 @@ public class MarketController extends Controller implements Observeur{
     }
 
     public void searchOffers(){
-        Offer offer1 = new Offer();
-        offer1.setOfferType(OfferType.LOAN);
-        offer1.setTitle("Un truc");
-        offer1.setPrice(100);
-        Offer offer2 = new Offer();
-        offer2.setOfferType(OfferType.SERVICE);
-        offer2.setTitle("Un autre truc");
-        offer2.setPrice(10);
-        offers.add(offer1);
-        offers.add(offer2);
+       for (Offer offer : listTest){
+            if(offer.getType()==this.type.toString()){
+                offers.add(offer);
+            }
+       }
     }
 
     public Pane makePaneOffre(Offer offer){
@@ -95,21 +114,25 @@ public class MarketController extends Controller implements Observeur{
         Label titleLabel = new Label(offer.getTitle());
         Label priceLabel = new Label(offer.getPrice() + " florains");
         String offerType;
-        switch(offer.getOfferType()){
-            case LOAN:
-                offerType = "Prêt";
+        switch(offer.getType()){
+            case "LOAN":
+                offerType = "Pret";
                 break;
-            case SERVICE:
+            case "SERVICE":
                 offerType = "Service";
                 break;
             default:
             offerType = "";
         }
         Label offerTypeLabel = new Label(offerType);
-        vbox.getChildren().addAll(titleLabel, priceLabel, offerTypeLabel);
+        Label ownerLabel = new Label("by : " + offer.getOwner());
+        Label frequencyLabel = new Label(offer.getFrequency());
+        vbox.getChildren().addAll(titleLabel, priceLabel, offerTypeLabel, ownerLabel, frequencyLabel);
         hbox.getChildren().addAll(image, vbox);
         pane.getChildren().add(hbox);
         pane.getStyleClass().add("offer");
+        titleLabel.getStyleClass().add("title");
+        priceLabel.getStyleClass().add("price");
         return pane;
     }
 }
