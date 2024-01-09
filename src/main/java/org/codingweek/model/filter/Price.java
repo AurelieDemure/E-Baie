f11e2ecf395d8ed22ff77014b0c5b3e38100d0fd
PierRapt <pierre.aug@orange.fr>
2024-01-09 13:58:50 +0100

    enum

---
+++ src/main/java/org/codingweek/model/filter/Price.java
@@ -0,0 +1,22 @@
+package org.codingweek.model.filter;
+
+public enum Price {
+    ALLPRICE("Tout type de prix"),
+    LESS100("Moins de 100 florains"),
+    FROM100TO200("Entre 100 et 200 florains"),
+    FROM200TO300("Entre 200 et 300 florains"),
+    FROM300TO400("Entre 300 et 400 florains"),
+    FROM400TO500("Entre 400 et 500 florains"),
+    MORE500("Plus de 500 florains");
+
+    private final String value;
+
+    Price(String value){
+        this.value = value;
+    }
+
+    public String getValue(){
+        return value;
+    }
+
+}
