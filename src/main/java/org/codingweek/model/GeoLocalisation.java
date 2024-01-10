a484f0252911bfbc41369bc15e72d105cfa95cf7
Kallu-A <lucas.aries@protonmail.com>
2024-01-10 16:19:04 +0100

    add: distance

--- src/main/java/org/codingweek/model/GeoLocalisation.java
+++ src/main/java/org/codingweek/model/GeoLocalisation.java
@@ -24,4 +24,22 @@ public class GeoLocalisation {
         if (response == null) return null;
         return response.getFirstPosition();
     }
+
+    /** Return the distance between two address in km
+     * Return -1 if address not valid*/
+    public static double distance(String address1, String address2) {
+        int rayon_earth = 6371;
+        JOpenCageLatLng latLng1 = getLatLng(address1);
+        JOpenCageLatLng latLng2 = getLatLng(address2);
+
+        if (latLng1 == null || latLng2 == null) return -1;
+
+
+        double a = Math.pow(Math.sin((latLng2.getLat() - latLng1.getLat()) / 2), 2)
+                + Math.cos(latLng1.getLat())
+                * Math.cos(latLng2.getLat())
+                * Math.pow(Math.sin((latLng2.getLng() - latLng1.getLng()) / 2), 2);
+
+        return rayon_earth * 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+    }
 }
--- src/test/java/GeoLocalisationTest.java
+++ src/test/java/GeoLocalisationTest.java
@@ -17,4 +17,13 @@ public class GeoLocalisationTest {
         assert firstResultLatLng.getLat().compareTo(48.67103) == 0;
         assert firstResultLatLng.getLng().compareTo(6.15083) == 0;
     }
+
+    @Test
+    public void DistanceTest() {
+        assert GeoLocalisation.distance("193, avenue Paul Muller BP 90172 Villers-lès-Nancy Cedex", "193, avenue Paul Muller BP 90172 Villers-lès-Nancy Cedex") == 0;
+        assert GeoLocalisation.distance("193, avenue Paul Muller BP 90172 Villers-lès-Nancy Cedex", "1, rue Grandville 54000 Nancy") > 0;
+        assert GeoLocalisation.distance("1 rue de Rungis, 75013 Paris", "193, avenue Paul Muller BP 90172 Villers-lès-Nancy Cedex") < 430;
+        assert GeoLocalisation.distance("1 rue de Rungis, 75013 Paris", "193, avenue Paul Muller BP 90172 Villers-lès-Nancy Cedex") > 370;
+        System.out.println(GeoLocalisation.distance("1 rue de Rungis, 75013 Paris", "193, avenue Paul Muller BP 90172 Villers-lès-Nancy Cedex"));
+    }
 }
