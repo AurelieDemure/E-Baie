dcafa9de43dab01180571f6226d831f21dda7ffa
TEMPESTINI Terry <terry.tempestini@telecomnancy.eu>
2024-01-10 16:10:47 +0100

    add: chat and messages

--- src/main/java/org/codingweek/controller/TchatController.java
+++ src/main/java/org/codingweek/controller/TchatController.java
@@ -2,22 +2,14 @@ package org.codingweek.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
+import javafx.scene.control.*;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import org.codingweek.ApplicationContext;
 import org.codingweek.db.entity.Chat;
-import org.codingweek.db.entity.Offer;
 import org.codingweek.db.entity.User;
 import org.codingweek.model.ChatModel;
 import org.codingweek.model.DatabaseHandler;
-import org.codingweek.model.ImageHandler;
-import org.codingweek.model.MarketModel;
 
 import java.net.URL;
 import java.util.*;
@@ -29,7 +21,7 @@ public class TchatController extends Controller implements Observeur {
     @FXML
     public VBox contact_list;
     @FXML
-    public VBox conversation;
+    public GridPane conversation;
 
     @FXML
     public TextArea message;
@@ -44,14 +36,28 @@ public class TchatController extends Controller implements Observeur {
     public String current_user;
     public String current_receiver;
 
+    @FXML
+    public ScrollPane scrollPaneConversation;
+
+    @FXML
+    public Button send;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+
         this.contacts.clear();
         this.chats.clear();
         this.current_user = ApplicationContext.getInstance().getUser_authentified().getEmail();
-        this.current_receiver = ChatModel.getContacts(current_user).get(0).getEmail();
 
-        refresh();
+        if(ChatModel.getContacts(current_user).size() > 0 && ChatModel.getContacts(current_user) != null){
+            this.current_receiver = ChatModel.getContacts(current_user).get(0).getEmail();
+            this.send.setDisable(false);
+            this.message.setDisable(false);
+            refresh();
+        }else{
+            this.send.setDisable(true);
+            this.message.setDisable(true);
+        }
     }
 
     public Date createDate(int hour, int minute) {
@@ -63,46 +69,18 @@ public class TchatController extends Controller implements Observeur {
         return calendar.getTime();
     }
 
-    public void testInitialise() {
-        User user1 = DatabaseHandler.getInstance().getDbManager().getEntity(User.class, "p");
-
-        /*User user2 = new User("example@example.com", "Jane", "Doe", "password123", "0701234567", "Paris", "Another description", 50, new Date());
-        User user3 = new User("test@test2.com","Test2", "User2", "test123","0601528495", "Nancy", "description", 100, new Date());
-
-        Chat chat1 = new Chat(user1, user2, "Hello", createDate(11, 0)); // 11:00
-        Chat chat2 = new Chat(user2, user1, "Hi", createDate(11, 20)); // 11:20
-
-        Chat chat7 = new Chat(user2, user1, "How are you?", createDate(11, 40)); // 11:40
-        Chat chat8 = new Chat(user1, user2, "I'm good, thanks!", createDate(12, 0)); // 12:00
-
-        Chat chat5 = new Chat(user2, user3, "Hey!", createDate(13, 0)); // 13:00
-        Chat chat6 = new Chat(user3, user2, "What's up?", createDate(13, 20)); // 13:20
-
-        DatabaseHandler.getInstance().getDbManager().saveEntity(user1);
-        DatabaseHandler.getInstance().getDbManager().saveEntity(user2);
-        DatabaseHandler.getInstance().getDbManager().saveEntity(user3);
-        DatabaseHandler.getInstance().getDbManager().saveEntity(chat1);
-        DatabaseHandler.getInstance().getDbManager().saveEntity(chat2);
-        DatabaseHandler.getInstance().getDbManager().saveEntity(chat5);
-        DatabaseHandler.getInstance().getDbManager().saveEntity(chat6);
-        DatabaseHandler.getInstance().getDbManager().saveEntity(chat7);
-        DatabaseHandler.getInstance().getDbManager().saveEntity(chat8);*/
-
-        /*User user1 = new User("e@example.com", "ggg", "Doe", "password123", "0701234567", "Paris", "Another description", 50, new Date());
-        User user2 = new User("f@test2.com","hhh", "User2", "test123","0601528495", "Nancy", "description", 100, new Date());*/
-
-        User user2 = DatabaseHandler.getInstance().getDbManager().getEntity(User.class, "e@example.com");
-        User user3 = DatabaseHandler.getInstance().getDbManager().getEntity(User.class, "f@test2.com");
-
-    }
-
     @Override
     public void refresh() {
+        int messageCounter = 0;
+
+        scrollPaneConversation.vvalueProperty().bind(conversation.heightProperty());
+
         this.contacts = ChatModel.getContacts(current_user);
         this.chats = ChatModel.getChats(current_user, current_receiver);
 
         this.contact_list.getChildren().clear();
         this.conversation.getChildren().clear();
+
         this.message.setText("");
 
         if(current_receiver != null){
@@ -119,22 +97,30 @@ public class TchatController extends Controller implements Observeur {
         }
 
         for(Chat chat: this.chats){
-            Pane pane = makeMessagePane(chat);
-            if(chat.getSender().getEmail().equals(current_user)){
-                pane.getStyleClass().add("message-sent");
+            Label label = new Label(chat.getMessage());
+
+            if (chat.getSender().getEmail().equals(current_user)) {
+                label.getStyleClass().add("user-message");
             } else {
-                pane.getStyleClass().add("message-received");
+                label.getStyleClass().add("other-message");
             }
-            this.conversation.getChildren().add(pane);
-        }
+
+            int column = chat.getSender().getEmail().equals(current_user) ? 1 : 0;
+            conversation.add(label, column, messageCounter);
+
+            messageCounter++;
+      }
     }
 
     public Button makeContactButton(User user){
         Button button = new Button();
         button.getStyleClass().add("contact-button");
-        button.setPrefWidth(150);
+        button.setPrefWidth(200);
         button.setPrefHeight(50);
-        button.setText(user.getEmail());
+        button.setText(user.getFirstName() + " " + user.getLastName());
+        if(this.current_receiver != null && this.current_receiver.equals(user.getEmail())){
+            button.getStyleClass().add("contact-button-selected");
+        }
         button.setOnAction((event) -> {
             this.current_receiver = user.getEmail();
             refresh();
@@ -142,29 +128,24 @@ public class TchatController extends Controller implements Observeur {
         return button;
     }
 
-    public HBox makeMessagePane(Chat conversation){
-        Label messageLabel = new Label(conversation.getMessage());
-        Label dateLabel = new Label(conversation.getDate().toString());
-
-        HBox hbox = new HBox();
-        hbox.getChildren().add(messageLabel);
-        hbox.getChildren().add(dateLabel);
-        return hbox;
-    }
-
     @Override
     public void update() {}
 
     @FXML
     public void sendMessage(ActionEvent actionEvent) {
-        String message = this.message.getText();
+        if(this.current_receiver != null){
+            String message = this.message.getText();
+            if (message.isEmpty()) {
+                return;
+            }
 
-        User sender = DatabaseHandler.getInstance().getDbManager().getEntity(User.class, current_user);
-        User receiver_user = DatabaseHandler.getInstance().getDbManager().getEntity(User.class, current_receiver);
+            User sender = DatabaseHandler.getInstance().getDbManager().getEntity(User.class, current_user);
+            User receiver_user = DatabaseHandler.getInstance().getDbManager().getEntity(User.class, current_receiver);
 
-        Chat chat = new Chat(sender, receiver_user, message, createDate(Calendar.getInstance().get(Calendar.HOUR_OF_DAY), Calendar.getInstance().get(Calendar.MINUTE)));
-        DatabaseHandler.getInstance().getDbManager().saveEntity(chat);
+            Chat chat = new Chat(sender, receiver_user, message, createDate(Calendar.getInstance().get(Calendar.HOUR_OF_DAY), Calendar.getInstance().get(Calendar.MINUTE)));
+            DatabaseHandler.getInstance().getDbManager().saveEntity(chat);
 
-        refresh();
+            refresh();
+        }
     }
 }
--- src/main/java/org/codingweek/model/ChatModel.java
+++ src/main/java/org/codingweek/model/ChatModel.java
@@ -1,32 +1,34 @@
 package org.codingweek.model;
 
 import org.codingweek.db.entity.Chat;
-import org.codingweek.db.entity.Notification;
-import org.codingweek.db.entity.Offer;
 import org.codingweek.db.entity.User;
-import org.codingweek.model.filter.Frequency;
-import org.codingweek.model.filter.OfferType;
-import org.codingweek.model.filter.Price;
-import org.codingweek.model.filter.SortOffer;
-
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class ChatModel {
-
-    private static List<User> users;
-    private static List<String> senders_messages;
-    private static List<String> receivers_messages;
 
     /** Return all the users that already interacted with the user */
     public static List<User> getContacts(String email) {
         List<Chat> chats = DatabaseHandler.getInstance().getDbManager().getAllEntity(Chat.class);
 
-        users = chats.stream()
-                .filter(chat -> chat.getReceiver().getEmail().equals(email) || chat.getSender().getEmail().equals(email))
-                .map(chat -> chat.getReceiver().getEmail().equals(email) ? chat.getSender() : chat.getReceiver())
+        if (chats == null) {
+            return Collections.emptyList();
+        }
+
+        List<User> users = chats.stream()
+                .filter(chat -> {
+                    User receiver = chat.getReceiver();
+                    User sender = chat.getSender();
+                    return receiver != null && sender != null &&
+                            (receiver.getEmail().equals(email) || sender.getEmail().equals(email));
+                })
+                .map(chat -> {
+                    User receiver = chat.getReceiver();
+                    User sender = chat.getSender();
+                    return receiver.getEmail().equals(email) ? sender : receiver;
+                })
                 .distinct()
                 .collect(Collectors.toList());
 
@@ -34,34 +36,24 @@ public class ChatModel {
     }
 
     /** Return all the messages between the user and the contact sorted by date */
-    public static List<String> getSenderMessages(String email, String contact) {
-        List<Chat> chats = DatabaseHandler.getInstance().getDbManager().getAllEntity(Chat.class);
-
-        senders_messages = chats.stream()
-                .filter(chat -> chat.getReceiver().getEmail().equals(email) && chat.getSender().getEmail().equals(contact))
-                .map(chat -> chat.getMessage())
-                .collect(Collectors.toList());
-        return senders_messages;
-    }
-
-    /** Return all the messages between the user and the contact sorted by date */
-    public static List<String> getReceiverMessages(String email, String contact) {
-        List<Chat> chats = DatabaseHandler.getInstance().getDbManager().getAllEntity(Chat.class);
-
-        receivers_messages = chats.stream()
-                .filter(chat -> chat.getReceiver().getEmail().equals(contact) && chat.getSender().getEmail().equals(email))
-                .map(chat -> chat.getMessage())
-                .collect(Collectors.toList());
-        return receivers_messages;
-    }
-
     public static List<Chat> getChats(String currentUser, String currentReceiver) {
         List<Chat> chats = DatabaseHandler.getInstance().getDbManager().getAllEntity(Chat.class);
 
-        return chats.stream()
-                .filter(chat -> chat.getReceiver().getEmail().equals(currentReceiver) && chat.getSender().getEmail().equals(currentUser) ||
-                        chat.getReceiver().getEmail().equals(currentUser) && chat.getSender().getEmail().equals(currentReceiver))
+        if (chats == null) {
+            return Collections.emptyList();
+        }
+
+        List<Chat> filteredChats = chats.stream()
+                .filter(chat -> {
+                    User receiver = chat.getReceiver();
+                    User sender = chat.getSender();
+                    return receiver != null && sender != null &&
+                            ((receiver.getEmail().equals(currentReceiver) && sender.getEmail().equals(currentUser)) ||
+                                    (receiver.getEmail().equals(currentUser) && sender.getEmail().equals(currentReceiver)));
+                })
                 .sorted(Comparator.comparing(Chat::getDate))
                 .collect(Collectors.toList());
+
+        return filteredChats;
     }
 }
