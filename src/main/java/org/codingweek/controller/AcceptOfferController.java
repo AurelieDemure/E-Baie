f5849191a9926aa96eb6fd7a4b9f4b6167e97d70
TEMPESTINI Terry <terry.tempestini@telecomnancy.eu>
2024-01-11 14:37:18 +0100

    add: contact button and styles

--- src/main/java/org/codingweek/controller/AcceptOfferController.java
+++ src/main/java/org/codingweek/controller/AcceptOfferController.java
@@ -18,6 +18,7 @@ import org.codingweek.model.DatabaseHandler;
 import org.codingweek.model.MyOffersModel;
 import org.codingweek.model.Page;
 import org.codingweek.view.MyOffersView;
+import org.codingweek.view.TchatView;
 
 import java.io.IOException;
 import java.net.URL;
@@ -49,10 +50,10 @@ public class AcceptOfferController extends Controller implements Observeur {
         content.getStyleClass().add("gridpane");
 
         ColumnConstraints defaultColumnConstraints = new ColumnConstraints();
-        defaultColumnConstraints.setPrefWidth(200);
-        defaultColumnConstraints.setPrefWidth(200);
-        defaultColumnConstraints.setPrefWidth(200);
-        defaultColumnConstraints.setPrefWidth(200);
+        defaultColumnConstraints.setPrefWidth(120);
+        defaultColumnConstraints.setPrefWidth(120);
+        defaultColumnConstraints.setPrefWidth(120);
+        defaultColumnConstraints.setPrefWidth(120);
 
         content.getColumnConstraints().add(defaultColumnConstraints);
 
@@ -85,12 +86,26 @@ public class AcceptOfferController extends Controller implements Observeur {
                 refresh();
             });
 
+            Button contact = new Button("Contact");
+            contact.getStyleClass().add("button_contact");
+            contact.setOnAction(event -> {
+                ApplicationContext.getInstance().setContactUser(query.getUser());
+                ApplicationContext.getInstance().setPageType(Page.MESSAGE);
+                try {
+                    ApplicationSettings.getInstance().setCurrentScene(new TchatView().loadScene());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            });
+
+
             content.add(firstName, 0, queries.indexOf(query));
             content.add(lastName, 1, queries.indexOf(query));
             content.add(beginDateLbl, 2, queries.indexOf(query));
             content.add(endDateLbl, 3, queries.indexOf(query));
             content.add(acceptBtn, 4, queries.indexOf(query));
             content.add(refuse, 5, queries.indexOf(query));
+            content.add(contact, 6, queries.indexOf(query));
         }
     }
 
