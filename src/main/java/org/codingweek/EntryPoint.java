a62bdb147d165d4b2800145fe94604b89a7bbc37
Kallu-A <lucas.aries@protonmail.com>
2024-01-11 10:41:22 +0100

    add dev forceentry

--- src/main/java/org/codingweek/EntryPoint.java
+++ src/main/java/org/codingweek/EntryPoint.java
@@ -3,21 +3,34 @@ package org.codingweek;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import org.codingweek.db.DatabaseManager;
+import org.codingweek.db.entity.User;
 import org.codingweek.model.DatabaseHandler;
 import org.codingweek.model.Page;
 import org.codingweek.view.ConnexionView;
+import org.codingweek.view.MarketView;
 import org.codingweek.view.OfferCreateView;
 
 import java.io.IOException;
 
 public class EntryPoint extends Application {
+
+    private final static boolean isDev = false;
+
+    private final static String emailDev = "dev@mail.com";
+
     @Override
     public void start(Stage stage) throws IOException {
         ApplicationSettings.getInstance().setPrimaryStage(stage);
         ApplicationContext.getInstance().setPageType(Page.NONE);
         ApplicationContext.getInstance().setUser_authentified(null);
         stage.setTitle(Configuration.APP_TITLE);
-        stage.setScene(new ConnexionView().loadScene());
+        if (!isDev) {
+            stage.setScene(new ConnexionView().loadScene());
+        } else {
+            ApplicationContext.getInstance().setUser_authentified(DatabaseHandler.getInstance().getDbManager().getEntity(User.class, emailDev));
+            stage.setScene(new MarketView().loadScene());
+        }
+
         stage.setResizable(false);
         stage.show();
         stage.setOnCloseRequest(e -> {
