613a9bdab826e54de412c8a7d01c66320ff0bf71
Kallu-A <lucas.aries@protonmail.com>
2024-01-12 11:27:09 +0100

    fix; nav issue

--- src/main/java/org/codingweek/controller/MyOffersController.java
+++ src/main/java/org/codingweek/controller/MyOffersController.java
@@ -27,7 +27,6 @@ public class MyOffersController extends Controller implements Observeur {
     public void initialize(URL location, ResourceBundle resources) {
         //DatabaseHandler.getInstance().getDbManager().addObserveur(this);
 
-        ApplicationContext.getInstance().setPageType(Page.OFFER);
 
         refresh();
     }
