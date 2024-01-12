d90d51dddad12de4be32b131b85482d2b8ea2f96
PierRapt <pierre.aug@orange.fr>
2024-01-12 11:17:29 +0100

    Ajout de note

--- src/main/java/org/codingweek/controller/MyQueriesController.java
+++ src/main/java/org/codingweek/controller/MyQueriesController.java
@@ -9,10 +9,18 @@ import org.codingweek.model.*;
 import org.codingweek.view.*;
 import javafx.event.*;
 import javafx.fxml.*;
+import javafx.scene.Group;
+import javafx.scene.Node;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.DialogPane;
 import javafx.scene.control.Label;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.*;
 import javafx.util.StringConverter;
 import javafx.util.converter.LocalDateStringConverter;
@@ -77,6 +85,7 @@ public class MyQueriesController extends Controller implements Observeur {
     }
 
     public Pane makePaneQuery(Query query) {
+        HBox notation = new HBox();
         ImageView starImage = new ImageView(new Image(Objects.requireNonNull(ApplicationContext.class.getResourceAsStream("/org/codingweek/img/star.png"))));
         starImage.setFitHeight(30);
         starImage.setFitWidth(30);
@@ -88,9 +97,12 @@ public class MyQueriesController extends Controller implements Observeur {
             noteLabel.setText(note + "/5");
         } else {
             noteLabel.setText("Ajouter une note");
+            notation.getStyleClass().add("note");
+            notation.setOnMouseClicked( (event -> notePopUp(event, query)));
         }
-        HBox notation = new HBox(noteLabel, starImage);
+        notation.getChildren().addAll(noteLabel, starImage);
         notation.getStyleClass().add("margin");
+        
 
         ImageView image = new ImageView();
         image.setImage(ImageHandler.getImage(query.getOffer().getPath()));
@@ -160,5 +172,53 @@ public class MyQueriesController extends Controller implements Observeur {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public void notePopUp(MouseEvent event, Query query) {
+        ToggleGroup note = new ToggleGroup();
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.getDialogPane().applyCss();
+        Node graphic = alert.getDialogPane().getGraphic();
+        alert.setDialogPane(new DialogPane() {
+            @Override
+            protected Node createDetailsButton() {
+                VBox vbox = new VBox();
+                RadioButton note0 = new RadioButton();
+                note0.setText("0");
+                note0.setToggleGroup(note);
+                note0.setOnAction(e -> query.setNotation(0));
+                RadioButton note1 = new RadioButton();
+                note1.setText("1");
+                note1.setToggleGroup(note);
+                note1.setOnAction(e -> query.setNotation(1));
+                RadioButton note2 = new RadioButton();
+                note2.setText("2");
+                note2.setToggleGroup(note);
+                note2.setOnAction(e -> query.setNotation(2));
+                RadioButton note3 = new RadioButton();
+                note3.setText("3");
+                note3.setToggleGroup(note);
+                note3.setOnAction(e -> query.setNotation(3));
+                RadioButton note4 = new RadioButton();
+                note4.setText("4");
+                note4.setToggleGroup(note);
+                note4.setOnAction(e -> query.setNotation(4));
+                RadioButton note5 = new RadioButton();
+                note5.setText("5");
+                note5.setToggleGroup(note);
+                note5.setOnAction(e -> query.setNotation(5));
+                vbox.getChildren().addAll(note0, note1, note2, note3, note4, note5);
+                return vbox;
+            }
+        });
+        alert.getDialogPane().getButtonTypes().addAll(ButtonType.APPLY, ButtonType.CANCEL);
+        alert.getDialogPane().setExpandableContent(new Group());
+        alert.getDialogPane().setExpanded(true);
+        alert.getDialogPane().setGraphic(graphic);
+        alert.setTitle("Notation");
+        alert.setHeaderText("Veuilliez attribuer une note à votre demande");
+        alert.showAndWait();
+
+        DatabaseHandler.getInstance().getDbManager().saveEntity(query);
     }
 }
